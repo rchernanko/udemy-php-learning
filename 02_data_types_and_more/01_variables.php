13f3e21f91c499f359e305090c119868e2456ab2
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-20 16:07:09 +0000

    adding variables video to initial commit

---
+++ 02_data_types_and_more/01_variables.php
@@ -0,0 +1,28 @@
+<?php
+    
+    //$ sign to define a variable, followed by it's name - can be a string or numbers
+    
+    $name;
+    $name2 = "Richard";    
+    $number = 100;
+    
+    echo $name2;
+    echo $number;
+    
+    //If I were to echo $name, i would see an error...
+    //Best practice for naming conventions - camelcase
+    
+    //Can store many different data types within the php variables:
+
+    $float = 100.5;
+
+    //Concatenation. Instead of the + in java, php uses a .
+
+    echo $name2 . " " . 200;
+
+    $name2 = "<h1> Hello </h1>";
+    //interestingly enough, I can have 2 variables of the same name inside the same file...
+
+    echo $name2;
+
+?>
