4457147e2caeda4074b552eeab0a5e6ff1c5f5de
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-20 08:51:13 +0000

    initial commit

---
+++ 01_first_steps/blank.php
@@ -0,0 +1,42 @@
+<!--
+
+Listening to the first parts of the course:
+
+1)
+
+Important to make a plan...how much i want to do per day?
+
+2)
+
+General info: 
+
+- PHP runs off a server.
+- There are many different softwares out there that we can use that will act as servers. We download it, run php and databases etc. This doesn't doesn't actually change anything on our computer 
+- We will use XAMP
+https://www.apachefriends.org/index.html
+
+3)
+
+the root directory for my server is XAMPP > htdocs directory
+
+i've added demo-2 folder (which is the exercises files downloaded)-->
+
+
+
+<!--Opening and closing tags in php files-->
+
+<!--If there is no html in the php file, the closing tag is redundant-->
+
+<?php
+
+//I can add a comment here, within the php
+
+/* I can add a block comment like this, again, within the php */
+
+echo "hello student";
+
+//I can also embed html in my php:
+
+echo "<h1>Hello</h1>";
+
+?>
---
+++ 01_first_steps/dynamic_text.php
@@ -0,0 +1,52 @@
+<!--
+
+Using dynamic data : 
+
+So let's imagine I have a website with 10 pages and I want the same h1 html tag on each page 
+e.g. Richard's Official Site.
+ 
+How can I make this dynamic, using php, so that I don't have to copy and paste the static "richard's official site" text on every page, but instead can have it just updated in 1 place?
+ 
+So before, the html would've looked like this:
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Richard's Offical Site</h1>
+    
+</body>
+</html>
+
+
+Now, let's make the h1 tag pull from dynamic data, by using php:
+
+- We will create a function that calls a variable defined somewhere else
+- So in the example below, this is a dirty, quick and easy example! I have created a title variable and assigned it the value I want.
+- In my h1 tag, I then call that variable
+- Of course, what would normally happen is that we would keep the variable in another separate file
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   <?php
+    
+    $title = "Richard's Official Site";
+    
+    ?>
+    
+    <h1><?php echo $title; ?></h1>
+    
+</body>
+</html>
