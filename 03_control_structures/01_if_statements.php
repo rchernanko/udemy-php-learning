b367327c348cd1194f87b1b8869707eaccf7c13a
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-21 08:58:10 +0000

    adding initial part of lesson on if statements

---
+++ 03_control_structures/01_if_statements.php
@@ -0,0 +1,22 @@
+<?php
+
+//If statements...
+
+if(3 > 10){
+    echo 'three is more than 10';
+} else {
+    echo 'it is not';
+}
+
+echo '<br>';
+//there is also an elseIf keyword in php:
+
+if(3 > 10){
+    echo 'three is more than 10';
+} elseif(3 < 10) {
+    echo 'three is less than 10';
+}
+
+//NEED TO FINISH THIS LESSON
+
+?>
