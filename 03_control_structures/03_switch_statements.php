ddbdb6ac745c8fef8f133c26deac59a0a35c817c
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-22 19:26:32 +0000

    adding switch statement lesson

---
+++ 03_control_structures/03_switch_statements.php
@@ -0,0 +1,26 @@
+<?php 
+
+//Switch statements
+
+$number = 65;
+
+switch($number) {
+    
+    case 34:
+        echo 'is it 34';
+        break;
+    case 10:
+        echo 'is it 10';
+        break;
+    case 134:
+        echo 'is it 134'; 
+        break;
+    case 65:
+        echo 'is it 65';
+        break;
+    default:
+        echo 'could not find anything';
+}
+
+
+?>
