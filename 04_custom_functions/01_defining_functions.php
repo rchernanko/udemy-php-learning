d6616cc66b675428e8e8876e4ddb636115b97b9c
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-23 12:26:53 +0000

    adding basic function call

---
+++ 04_custom_functions/01_defining_functions.php
@@ -0,0 +1,32 @@
+<?php 
+
+//Defining functions
+
+//I'll define a function below:
+
+function saySomething() {
+    echo 'hello student';
+}
+
+//And now let me call it:
+
+saySomething();
+
+echo '<br>';
+echo '<br>';
+
+//Of course, similar to java you can call functions within functions:
+
+function initialise() {
+    echo 'initialising';
+    calculate();
+}
+
+function calculate() {
+    echo 2+3;
+}
+
+initialise();
+//It doesn't seem to matter where i call the initialise function within this file, it will always be called. i.e. i could have it at the top of this file and it would still work ok.
+
+?>
