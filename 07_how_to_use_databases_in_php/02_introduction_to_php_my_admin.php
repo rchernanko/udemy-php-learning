bfbd78164a2d37e8c4fbad740bbd527d8822b9d0
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-23 18:52:42 +0000

    introduction to php my admin

---
+++ 07_how_to_use_databases_in_php/02_introduction_to_php_my_admin.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+
+PHPMyAdmin is a tool that allows us to manage databases...
+
+Need to get the apache web server and mysql database up and running in XAMPP
+
+Then type in http://localhost/phpmyadmin/ in the browser
+
+Edwin says that to understand PhpMyAdmin in more detail, it's worth taking another course ha! For example - https://www.udemy.com/php-database-connection-to-mysql/
+
+TODO - have a look around for courses at some stage
+
+Anyway, as part of this course, we will probably use only the 'Database' and 'Users' tabs...
+
+*/
