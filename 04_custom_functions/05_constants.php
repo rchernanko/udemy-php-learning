ce49c59f3d3a0392710f8e7ee9a140de554c6661
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-23 13:37:34 +0000

    adding constants lesson

---
+++ 04_custom_functions/05_constants.php
@@ -0,0 +1,16 @@
+<?php
+
+//Constants
+
+//normal variable
+$number = 10;
+echo $number;
+echo '<br>';
+
+//to define a constant, we do the below using the built-in define function:
+
+define('name', 'richard');
+//first part is the variable name and the second part is its constant value
+
+echo name;
+
