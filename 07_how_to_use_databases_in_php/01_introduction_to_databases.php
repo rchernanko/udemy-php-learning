16644be98c2a4f320d2e4eec9a404eedc7495f12
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-23 18:43:29 +0000

    adding screenshot of an example of a database + adding intro to db lesson

---
+++ 07_how_to_use_databases_in_php/01_introduction_to_databases.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+
+What is a database?
+
+- Simply a colleciton of related data in an organised format
+
+- We will be using MYSQL as part of this course
+- SQL stands for Structured Query Language
+
+- Really good example (see image in this repo) of what the following things are in a database:
+
+ * Column
+ * Row
+ * Field
+ 
+ 
+*/
