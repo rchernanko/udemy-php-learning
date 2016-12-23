a0b60e225bf9265e1cf0f7fe68baaaed3cd8b799
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-23 13:53:54 +0000

    adding string functions lesson

---
+++ 05_php_built_in_functions/02_string_functions.php
@@ -0,0 +1,23 @@
+<?php
+
+//Built in string functions
+//Obviously I will need to play around with this in a little more detail
+
+//Good references - http://php.net/manual/en/ref.strings.php
+
+$string = 'Hello student do you like the class';
+
+//string length
+echo strlen($string);
+echo '<br>';
+
+//string to upper case
+echo strtoupper($string);
+echo '<br>';
+
+//string to upper case
+echo strtolower($string);
+echo '<br>';
+
+//he says in the tutorials = there's loads of things in the php docs.
+//need to go through it at some stage
