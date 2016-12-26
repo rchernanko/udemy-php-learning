d5f1bef96a3f41f03c94ed2e9e7def0904a42c8b
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-26 21:56:20 +0000

    adding print vs echo

---
+++ 11_pluralsight_course/09_print_vs_echo.php
@@ -0,0 +1,12 @@
+<?php
+
+print("This above all: to thine own self be true");
+
+//or
+
+print "This above all: to thine own self be true";
+
+//1 thing that echo can do that print cannot do - it can print more than 1 parameter
+echo "hello ", "there ,", "you";
+
+//doesn't actually matter what you use (print or echo) - more important to be consistent...
