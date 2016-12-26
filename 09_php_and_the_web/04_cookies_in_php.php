a99db3fc4255278dd52e67990d34b1eea8baaf01
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-12-26 14:32:04 +0000

    adding introduction to cookies

---
+++ 09_php_and_the_web/04_cookies_in_php.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+Cookies...
+
+- Roughly how do they work... (assuming that my browser accepts cookies)
+- Well, when I visit that website, the website is then able to send a request to set a cookie into my browser
+- Allows personalisation etc
+- When you go to the site again, your browser cookies are sent (in the request header) to the website's servers which
+can then send back some data to personalise your experience on their site (in your browser)...
+
+Let's set some cookies in the next lesson...
+
+*/
